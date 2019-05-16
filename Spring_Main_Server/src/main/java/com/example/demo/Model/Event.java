94c33d1ea6e68a66fb0ded70c53e0fb56052626b
Federico-pomponii <federico.pomponii@gmail.com>
2019-05-16 13:13:53 +0200

    Added Main Server Mock Test

--- Spring_Main_Server/src/main/java/com/example/demo/Model/Event.java
+++ Spring_Main_Server/src/main/java/com/example/demo/Model/Event.java
@@ -1,24 +1,25 @@
 package com.example.demo.Model;
 
 /////////////////////////////////////////////////////////////////////////// EXAMPLE OF EVENT CURL
-// curl -X POST \
-//  http://localhost:8080/new \
-//  -H 'cache-control: no-cache' \
-//  -H 'content-type: application/json' \
-//  -d '{
-//    # id
-//    "type": "PressureChanged",
-//    "event_schema_revision": 1,
-//    "payload": {
-//        "board_id": "cafebafe-cafebabe-cafebabe",
-//        "board_offset": 3
-//        "bar": 5,
-//        "emitted_at": 857671257612
-//    }
-//    "topic_id": "UUID",
-//    # topic_height
-//    # received_at
-//}'
+ /*curl -X POST \
+  http://localhost:8080/new \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -d '{
+    # id
+    "type": "PressureChanged",
+    "event_schema_revision": 1,
+    "payload": {
+        "board_id": "cafebafe-cafebabe-cafebabe",
+       "board_offset": 3
+        "bar": 5,
+        "emitted_at": 857671257612
+    }
+    "topic_id": "UUID",
+    # topic_height
+    # received_at
+}'
+ */
 ///////////////////////////////////////////////////////////////////////////
 
 import com.example.demo.MyException;
